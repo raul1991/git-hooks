e72a662f2ced878bbd3207e8420cf984e6320a2c
cafebabe1991 <supremeanitabawa@gmail.com>
2017-06-16 08:51:02 +0530

    Updated the linter script
    Unwanted command outputs redirected to /dev/null

--- scripts/linter.sh
+++ scripts/linter.sh
@@ -10,25 +10,33 @@ output=$1
 git diff --check
 whiteSpaceErrors=$?
 
-git diff --cached --check 
+git diff --cached --check
 whiteSpaceErrors=$(($whiteSpaceErrors+$?))
 echo Count = $whiteSpaceErrors
+
+is_python_installed=$(python --version &> /dev/null)
+
+if [ $? -ne 0 ]; then
+   echo "Python is missing. Please install it or use branch hooks-no-dependency"
+   exit 1
+fi
+
 
 if [ "$whiteSpaceErrors" = 0 ]; then
    clear
    echo "Everything is fine"
    exit 0
-else 
+else
    #the cmd that generates a list of file to run the script on
    cmd=`$output | grep -Eo "^([a-zA-Z._/]*/*\.[a-zA-Z]*)|([a-zA-Z._/]*/[a-zA-Z./_]*/*\.[a-zA-Z]*)"`
 
    #lint every file for spaces and tab spaces.
    for f in $cmd
    do
-	#format using the remove whitespaces command
-	python remove_ws.py -f "$f" -c TS TB
-	clear
-	echo "White spaces removed - Add files again"
-	exit 1
-   done 
+    #format using the remove whitespaces command
+    python scripts/remove_ws.py -f "$f" -c TS TB
+    clear
+    echo "White spaces removed - Add files again"
+    exit 1
+   done
 fi
