5a65662515af8f27ee2c4c847f8965156a6c3537
erahbaw <rahul.bawa@ericsson.com>
2017-06-16 12:05:50 +0530

    Create an init script for the hooks setup
    Fixes https://github.com/raul1991/git-hooks/issues/2

---
+++ init.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+#env variables
+export GIT_DIR=".git"
+
+bool_glob=false
+
+scripts_dir="scripts/"
+hooks_dir="hooks/"
+
+function check_dir
+{
+  if [ -d '$GIT_DIR/$scripts_dir' ]; then
+	bool_glob=true
+  else
+	bool_glob=false
+  fi
+
+}
+
+function start_script
+{
+echo "Do you want to install the following hook ?(y/n)"
+read choice
+
+if [[ $choice == "y" || $choice == "Y" || $choice == "Yes" ]]; then
+   echo "Installing..."
+   check_dir
+   if ( $bool_glob ); then
+
+   	mkdir $GIT_DIR/$scripts_dir
+   fi
+
+   cp -R $scripts_dir/* $GIT_DIR/$scripts_dir/
+
+   echo "Scripts copied..."
+   
+   cp -R $hooks_dir/* $GIT_DIR/$hooks_dir/
+
+   echo "Hooks copied"
+
+fi
+}
+
+start_script
